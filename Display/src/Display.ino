d50306acc166be9f775a0fe814cadca5059ef22a
David <dmarquez.estudis@gmail.com>
2020-10-18 20:27:56 +0200

    Get data from EnergiData and display it

--- Display/src/Display.ino
+++ Display/src/Display.ino
@@ -37,8 +37,39 @@ void setup()
     display.setCursor(0, 0);
     display.printlnf("Temperature: %d", 23);
     display.display();
+    Particle.subscribe("hook-response/greenproduction", handleGreenProduction, MY_DEVICES);
 }
 
 void loop()
 {
+    Particle.publish("greenproduction", PRIVATE);
+    delay(5000);
+}
+void handleGreenProduction(const char *event, const char *data)
+{
+    display.clearDisplay();
+    display.setCursor(0, 0);
+
+    float total, biomass, solarpower, offshorewindpower;
+
+    // %f doesn't work in particle: https://github.com/particle-iot/device-os/issues/1124
+    // The current workaround is to do it as a string
+    //int result = sscanf(data, "%[^~]~%[^~]~%[^~]~%[^~]", &total, &biomass, &solarpower, &offshorewindpower);
+    //Log.info("%s", data);
+    //Log.info("%d", result);
+
+    //Idea from https://community.particle.io/t/tutorial-webhooks-and-responses-with-parsing-json-mustache-tokens/14612/21
+    String str = String(data);
+    char strBuffer[500] = "";
+    str.toCharArray(strBuffer, 500);
+    total = String(strtok(strBuffer, "~")).toFloat();
+    biomass = String(strtok(NULL, "~")).toFloat();
+    solarpower = String(strtok(NULL, "~")).toFloat();
+    offshorewindpower = String(strtok(NULL, "~")).toFloat();
+    
+    display.printlnf("Total: %.2f", total);
+    display.printlnf("Biomass: %.2f", biomass);
+    display.printlnf("Solar power: %.2f", solarpower);
+    display.printlnf("Off. Wind: %.2f", offshorewindpower);
+    display.display();
 }
