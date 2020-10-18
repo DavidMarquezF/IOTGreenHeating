9257344cf1cf9eb1dbc12559a942672de09e8dd7
David <dmarquez.estudis@gmail.com>
2020-10-18 14:20:00 +0200

    First working version of the display

---
+++ Display/src/Display.ino
@@ -0,0 +1,38 @@
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+#define SCREEN_WIDTH (uint8_t)128 // OLED display width, in pixels
+#define SCREEN_HEIGHT (uint8_t)64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI
+#define OLED_MOSI (int8_t) D0
+#define OLED_CLK (int8_t) D1
+#define OLED_DC (int8_t) D2
+#define OLED_RESET (int8_t) D3
+#define OLED_CS (int8_t) D4
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+SerialLogHandler logHandler;
+
+void setup()
+{
+    if (!display.begin(SSD1306_SWITCHCAPVCC))
+    {
+        Log.info("SSD1306 allocation failed");
+        //TODO: Stop the execution of the program
+    }
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+    display.clearDisplay();
+    // Show the display buffer on the screen. You MUST call display() after
+    // drawing commands to make them visible on screen!
+    display.display();
+}
+
+void loop()
+{
+}
---
+++ Display/target/1.5.2/photon/obj/src/module_info.o.d
@@ -0,0 +1,48 @@
+/home/david/Documents/IOT/IOTGreenHeating/Display/target/1.5.2/photon//obj/src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/assert.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_ansi.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/newlib.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_newlib_version.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/config.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/ieeefp.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/features.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/stdint.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/_default_types.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_intsup.h \
+ /home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_stdint.h
+
+../../../dynalib/inc/module_info.inc:
+
+../../../dynalib/inc/module_info.h:
+
+../../../services/inc/static_assert.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/assert.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_ansi.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/newlib.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_newlib_version.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/config.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/features.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/stdint.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/_default_types.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_intsup.h:
+
+/home/david/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_stdint.h:
